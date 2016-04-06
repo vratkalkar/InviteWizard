65f31ac7204c74c05dc9def3e1d1a0d82f6252af
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-06 16:41:39 -0400

    update page structure and add stylesheet links

--- phpform/invite_templates/save_the_date_template.php
+++ phpform/invite_templates/save_the_date_template.php
@@ -1,8 +1,10 @@
 <html>
 <meta charset="utf-8">
 <head>
-</head>
-<body>
-	<div class="logo"></div>
-</body>
+  <link rel="stylesheet" href="main.css" type="text/css">
+  <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
+ </head>
+ <body>
+   <div class="logo"></div>
+ </body>
 </html>
