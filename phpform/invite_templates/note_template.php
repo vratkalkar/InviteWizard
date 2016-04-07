3276147d66b0e56438ca13a254bedd65b95d3f50
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-07 00:31:36 -0400

    Center index h1

--- phpform/invite_templates/note_template.php
+++ phpform/invite_templates/note_template.php
@@ -5,12 +5,12 @@
   <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
  </head>
  <body>
-   <div class="page_title_region">appy couple</div>
-   <div class="row">
+   <div class="title_bar">appy couple</div>
+   <!-- <div class="row">
      <div class="column small-12 medium-6 large-4">Column 1</div>
    	 <div class="column small-12 medium-6 large-4">Column 2</div>
    	 <div class="column small-12 medium-6 large-4">Column 3</div>
-   </div>
+   </div> -->
 
    <div id="theme_preview_area">
    </div>
