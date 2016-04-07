12a6cca730afe92b67d942a9af4fba7cacfea0f7
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-07 17:26:19 -0400

    Fix path to main.css for template folder

--- phpform/invite_templates/save_the_date_template.php
+++ phpform/invite_templates/save_the_date_template.php
@@ -1,7 +1,7 @@
 <html>
 <meta charset="utf-8">
 <head>
-  <link rel="stylesheet" href="main.css" type="text/css">
+  <link rel="stylesheet" href="../main.css" type="text/css">
   <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
  </head>
  <body>
