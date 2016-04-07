daa8111474be2bfe065733bfdce24136e59a27c8
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-06 23:07:00 -0400

    Add additional template files as outlined in pdf

--- phpform/invite_templates/app_website_info_template.php
+++ phpform/invite_templates/app_website_info_template.php
@@ -1,9 +1,17 @@
 <html>
 <meta charset="utf-8">
 <head>
-</head>
-<body>
-	<div class="logo"></div>
-</body>
+  <link rel="stylesheet" href="main.css" type="text/css">
+  <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
+ </head>
+ <body>
+   <div class="logo"></div>
+   <div class="row">
+     <div class="column small-12 medium-6 large-4">Column 1</div>
+   	 <div class="column small-12 medium-6 large-4">Column 2</div>
+   	 <div class="column small-12 medium-6 large-4">Column 3</div>
+   </div>
+    <div id="theme_preview_area">
+   	</div>
+ </body>
 </html>
-<div class="logo"></div>
