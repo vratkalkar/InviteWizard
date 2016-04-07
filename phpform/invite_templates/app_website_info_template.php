1652d1e5376fd25e1506ac22da6b2f472792af00
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-07 00:02:58 -0400

    Add css class to add page header color and font as shown in outline pdf

--- phpform/invite_templates/app_website_info_template.php
+++ phpform/invite_templates/app_website_info_template.php
@@ -5,13 +5,14 @@
   <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
  </head>
  <body>
-   <div class="logo"></div>
+   <div class="page_title_region"></div>
    <div class="row">
      <div class="column small-12 medium-6 large-4">
-     	<a class="th">Column 1</a>
+     	<div class="page_title_region"></div>
+
      </div>
-   	 <div class="column small-12 medium-6 large-4">Column 2</div>
-   	 <div class="column small-12 medium-6 large-4">Column 3</div>
+   	 <div class="column small-12 medium-6 large-4"></div>
+   	 <div class="column small-12 medium-6 large-4"></div>
    </div>
     <div id="theme_preview_area">
     	<p>Hello</p>
---
+++ phpform/invite_templates/reminder_template.php
@@ -0,0 +1,18 @@
+<html>
+<meta charset="utf-8">
+<head>
+  <link rel="stylesheet" href="main.css" type="text/css">
+  <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
+ </head>
+ <body>
+   <div class="page_title_region"></div>
+   <div class="row">
+     <div class="column small-12 medium-6 large-4">Column 1</div>
+   	 <div class="column small-12 medium-6 large-4">Column 2</div>
+   	 <div class="column small-12 medium-6 large-4">Column 3</div>
+   </div>
+
+   <div id="theme_preview_area">
+   </div>
+ </body>
+</html>
