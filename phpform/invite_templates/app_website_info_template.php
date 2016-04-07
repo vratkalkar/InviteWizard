15615ba46dd2e6c142503f40b69246ef6f9c61af
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-06 23:37:24 -0400

    Add css columns to newly added template files

--- phpform/invite_templates/app_website_info_template.php
+++ phpform/invite_templates/app_website_info_template.php
@@ -7,11 +7,14 @@
  <body>
    <div class="logo"></div>
    <div class="row">
-     <div class="column small-12 medium-6 large-4">Column 1</div>
+     <div class="column small-12 medium-6 large-4">
+     	<a class="th">Column 1</a>
+     </div>
    	 <div class="column small-12 medium-6 large-4">Column 2</div>
    	 <div class="column small-12 medium-6 large-4">Column 3</div>
    </div>
     <div id="theme_preview_area">
+    	<p>Hello</p>
    	</div>
  </body>
 </html>
