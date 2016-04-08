4d6b29c89ee76e82a267b2b98293fb99ee854405
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-08 15:02:12 -0400

    Add template html/css structure to new template.

---
+++ phpform/invite_templates/appy_suite_template.php
@@ -0,0 +1,17 @@
+<html>
+<meta charset="utf-8">
+<head>
+  <link rel="stylesheet" href="../main.css" type="text/css">
+  <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
+ </head>
+ <body class="template_background">
+   <div class="title_bar">APPY COUPLE</div>
+   <div class="sub_title_bar">Appy Stationery Studio</div>
+   <div class="row">
+     <div class="column small-12 medium-6 large-4"></div>
+   	 <div class="column small-12 medium-6 large-4"></div>
+   	 <div class="column small-12 medium-6 large-4"></div>
+   </div>
+   	</div>
+ </body>
+</html>
