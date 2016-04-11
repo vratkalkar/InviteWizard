43a9933c2c22bdc647dd23744942eedffe00bd73
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-11 18:41:19 -0400

    Add basic html structure with css link tags and js script tags

--- phpform/display_customized_templates/invitation.php
+++ phpform/display_customized_templates/invitation.php
@@ -1,3 +1,16 @@
 <?php
 session_start();
 ?>
+<meta charset="utf-8">
+<head>
+  <link rel="stylesheet" href="../main.css" type="text/css">
+  <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.min.css" type="text/css">
+ </head>
+ <body class="template_background">
+     <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"
+         integrity="sha256-xNjb53/rY+WmG+4L6tTl9m6PpqknWZvRt0rO1SRnJzw="
+         crossorigin="anonymous">
+     </script>
+ </body>
+</html>
