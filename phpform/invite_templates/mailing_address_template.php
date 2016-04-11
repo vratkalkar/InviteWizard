e02cc1f18bb10a9cd2bda6c42e086cf9dfb05a80
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-11 14:45:16 -0400

    Remove typo on mail address page

--- phpform/invite_templates/mailing_address_template.php
+++ phpform/invite_templates/mailing_address_template.php
@@ -3,14 +3,13 @@ session_start();
 $_SESSION['address'] = $POST['address'];
 $_SESSION['email_address'] = $POST['email_address'];
 $_SESSION['phone_number'] = $POST['phone_number'];
-
 ?>
 <html>
 <meta charset="utf-8">
 <head>
   <link rel="stylesheet" href="../main.css" type="text/css">
   <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.min.css" type="text/css">s
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.min.css" type="text/css">
  </head>
  <body class="template_background">
    <div class="title_bar">APPY COUPLE</div>
