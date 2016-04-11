a3e27166129916bbd1c7bdfe06a1359c1255df6c
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-11 15:50:08 -0400

    Rename invite_template folder to customizable_templates and updated redirect file path

--- phpform/process_index_selection_form.php
+++ phpform/process_index_selection_form.php
@@ -5,25 +5,25 @@ if(isset($_POST['form_submit'])){
 	$user_choice = $_POST['chosen_template'];
 	 switch($user_choice){
 	 	case 'appy_stationery_pack':
-	 	  header('Location: ../invite_templates/appy_suite_template.php');
+	 	  header('Location: ../customizable_templates/appy_suite_template.php');
 	 	  break;
 		case 'wedding_invitation':
-		  header('Location: ../invite_templates/customize_invitation.php');
+		  header('Location: ../customizable_templates/customize_invitation.php');
 		  break;
 		case 'mailing_address':
-		  header("Location: ../invite_templates/customize_mailing_address.php");
+		  header("Location: ../customizable_templates/customize_mailing_address.php");
 		  break;
 		case 'save_the_date':
-		  header("Location: ../invite_templates/customize_save_the_date.php");
+		  header("Location: ../customizable_templates/customize_save_the_date.php");
 		  break;
 		case 'notes':
-		  header("Location: ../invite_templates/customize_note.php");
+		  header("Location: ../customizable_templates/customize_note.php");
 		  break;
 		case 'reminder':
-		  header("Location: ../invite_templates/customize_reminder.php");
+		  header("Location: ../customizable_templates/customize_reminder.php");
 		  break;
 		case 'thank_you_card':
-		  header("Location: ../invite_templates/customize_thank_you_card.php");
+		  header("Location: ../customizable_templates/customize_thank_you_card.php");
 		  break;
 	}
 
