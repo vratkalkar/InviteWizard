63aa792c38995c698ac4c0d2d47de4dbf53a2e0d
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-12 17:29:47 -0400

    Revert ckeditor to cdn

--- phpform/main.js
+++ phpform/main.js
@@ -1,13 +1,10 @@
-//Customizable Template TextAreas to CKEditor
-CKEDITOR.replace('message');
-CKEDITOR.replace('address');
-CKEDITOR.replace('note_message');
-CKEDITOR.replace('reminder');
-CKEDITOR.replace('save_the_date_message');
-CKEDITOR.replace('thank_you_message');
-
-//Increase CKEditor height and width
-CKEDITOR.config.resize_enabled = false;
+// //Customizable Template TextAreas to CKEditor
+// CKEDITOR.replace('message');
+// CKEDITOR.replace('address');
+// CKEDITOR.replace('note_message');
+// CKEDITOR.replace('reminder');
+// CKEDITOR.replace('save_the_date_message');
+// CKEDITOR.replace('thank_you_message');
 
 
 
