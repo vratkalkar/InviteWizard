d8b538fc3edc6bd74a04da6206dc1401d581bec5
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-12 14:49:22 -0400

    Added basic CKEditor

--- phpform/customizable_templates/customize_invitation.php
+++ phpform/customizable_templates/customize_invitation.php
@@ -32,7 +32,7 @@ $_SESSION['date'] = $_POST['date'];
             <input name="spouse2" required/>
           </label>
           <label>Add a message:<br>
-          <textarea name="message" required></textarea>
+          <textarea class="editor" name="message" required></textarea>
           <label>Venue:<br>
             <input name="venue" required/>
           </label>
@@ -45,10 +45,7 @@ $_SESSION['date'] = $_POST['date'];
    	 <div class="column small-12 medium-6 large-4"></div>
    	 <div class="column small-12 medium-6 large-4"></div>
    </div>
-     <script src="https://cdn.ckeditor.com/4.5.8/standard/ckeditor.js"></script>
-     <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"
-         integrity="sha256-xNjb53/rY+WmG+4L6tTl9m6PpqknWZvRt0rO1SRnJzw="
-         crossorigin="anonymous">
-     </script>
+     <script src="http://cdn.ckeditor.com/4.5.8/standard/ckeditor.js"></script>
+     <script type="text/javascript" src="../main.js"></script>
  </body>
 </html>
--- phpform/main.js
+++ phpform/main.js
@@ -1,2 +1,7 @@
-//Customize Invitation TextArea
+//Customizable Template TextAreas to CKEditor
 CKEDITOR.replace('message');
+CKEDITOR.replace('address');
+CKEDITOR.replace('note_message');
+CKEDITOR.replace('reminder');
+CKEDITOR.replace('save_the_date_message');
+CKEDITOR.replace('thank_you_message');
