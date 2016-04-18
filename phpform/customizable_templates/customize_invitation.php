1c809244a97bcdaeb8695e92f7a3b08e49d1b971
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-18 09:21:25 -0400

    Start adding invite_type select to session

--- phpform/customizable_templates/customize_invitation.php
+++ phpform/customizable_templates/customize_invitation.php
@@ -12,6 +12,7 @@ $spouse1 = sanitize($_POST['spouse1']);
 $spouse2 = sanitize($_POST['spouse2']);
 $venue = sanitize($_POST['venue']);
 $date = sanitize($_POST['date']);
+$invite_type = $_POST['invite_type'];
 
 
 $_SESSION['parents_names'] = $parents_names;
@@ -19,6 +20,7 @@ $_SESSION['spouse1'] = $spouse1;
 $_SESSION['spouse2'] = $spouse2;
 $_SESSION['venue'] = $venue;
 $_SESSION['date'] = $date;
+$_SESSION['invite_type'] = $invite_type;
 
 $buffer = ob_get_contents();
 ob_end_clean();
