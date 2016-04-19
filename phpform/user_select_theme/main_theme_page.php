fca0b50822a1a714c4ac356c3c845eebef9d3d85
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-19 08:24:39 -0400

    Change theme selection page extension to .php && add basic html/css/js structure

---
+++ phpform/user_select_theme/main_theme_page.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+
+<html>
+<meta charset="utf-8">
+<head>
+  <link rel="stylesheet" href="../main.css" type="text/css">
+  <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css"> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.min.css" type="text/css">
+ </head>
+ <body class="template_background">
+ <div class="title_bar">APPY COUPLE</div>
+   <div class="sub_title_bar">Appy Stationery Studio</div>
+ 	<p>This is the theme selection page</p>
+ 	<?php 
+ 	 var_dump($_SESSION);
+ 	?>
+ 	<p></p>
+ </body>
+</html>
