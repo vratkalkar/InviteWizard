f3facb761736d5a72ca764829deab20b23b37286
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-19 11:13:51 -0400

    Working to fix main_theme_page.php button class

--- phpform/user_select_theme/main_theme_page.php
+++ phpform/user_select_theme/main_theme_page.php
@@ -12,15 +12,18 @@ session_start();
  <body class="template_background">
  <div class="title_bar">APPY COUPLE</div>
    <div class="sub_title_bar">Appy Stationery Studio</div>
-    <div class="header_section">
- 	  <h3>Choose a Theme</h3>
-    </div>
-    <div class="theme_container">
-    	<p style="text-align:center; padding-top: 200px">Theme images would appear here for selection</p>
-    </div>
- 	<?php 
- 	 var_dump($_SESSION);
- 	?>
+    <div class="element_container">
+     <div class="header_section">
+ 	   <h3>Choose a Theme</h3>
+     </div>
+
+      <div class="theme_container">
+      	<form action="process_theme_selection.php" method="POST">
+    	  <p style="text-align:center; padding-top: 200px">Theme images would appear here for selection</p>
+      </div>
+      <input class="index_button" class="expanded button" type="submit" name="form_submit" value="Customize">
+    </form>
+</div>
  	<p></p>
  </body>
 </html>
