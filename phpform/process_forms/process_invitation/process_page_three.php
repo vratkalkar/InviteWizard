ac661cb9fe3c9fb6d334864b35712bca68f83c3d
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-25 18:17:28 -0400

    Delete process_invitation.php and move contents to process_page_three.php

--- phpform/process_forms/process_invitation/process_page_three.php
+++ phpform/process_forms/process_invitation/process_page_three.php
@@ -11,6 +11,14 @@ if(isset($_POST['form_submit'])){
 	$_SESSION['time'] = sanitize($_POST['time']);
 	$_SESSION['website_url'] = sanitize($_POST['website_url']);
 	$_SESSION['wedding_code'] = sanitize($_POST['wedding_code']);
-	header("Location: ../../display_customized_templates/invitation_type/formal_invitation.php");
+	
+	$user_choice = $_POST['invite_type'];
+    switch ($user_choice) {
+      case 'formal':
+	    header("Location: ../display_customized_templates/invitation_type/formal_invitation.php");
+	    break;
+      case 'casual':
+	    header("Location: ../display_customized_templates/invitation_type/casual_invitation.php");
+	    break;
 }
 ?>
