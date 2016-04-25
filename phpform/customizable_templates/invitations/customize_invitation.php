29011c15dedc72c909b34d7a0a842ff39d5d7043
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-25 16:16:05 -0400

    Fix relative path to main.css and foundation css

--- phpform/customizable_templates/invitations/customize_invitation.php
+++ phpform/customizable_templates/invitations/customize_invitation.php
@@ -13,9 +13,9 @@ $_SESSION['wedding_code'] = $_POST['wedding_code'];
 <html>
 <meta charset="utf-8">
 <head>
-  <link rel="stylesheet" href="../reset.css" type="text/css" />
-  <link rel="stylesheet" href="../main.css" type="text/css" />
-  <link rel="stylesheet" href="../foundation-6.2.0-essential/css/foundation.css" type="text/css" /> 
+  <link rel="stylesheet" href="../../reset.css" type="text/css" />
+  <link rel="stylesheet" href="../../main.css" type="text/css" />
+  <link rel="stylesheet" href="../../foundation-6.2.0-essential/css/foundation.css" type="text/css" /> 
  </head>
  <body class="template_background">
    <div class="title_bar">APPY COUPLE</div>
