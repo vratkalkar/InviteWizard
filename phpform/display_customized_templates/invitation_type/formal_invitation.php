c0c811c6f058f086d45efbc03a7ab10ad4acd088
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-25 17:30:35 -0400

    Change button_area class buttons to appy red

--- phpform/display_customized_templates/invitation_type/formal_invitation.php
+++ phpform/display_customized_templates/invitation_type/formal_invitation.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("../helper_functions/sanitize.php");
+include("../../helper_functions/sanitize.php");
 $_SESSION['parents_names'] = sanitize($_POST['parents_names']);
 $_SESSION['spouse1'] = sanitize($_POST['spouse1']);
 $_SESSION['spouse2'] = sanitize($_POST['spouse2']);
@@ -27,12 +27,11 @@ $_SESSION['wedding_code'] = sanitize($_POST['wedding_code']);
      <h3>Your Suggested Invitation:</h3>
    </div>
    <?php var_dump($_SESSION); ?>
-     <script type="text/javascript" src="../main.js"></script>
  </body>
-</html>
+ 	<div class="button_area">
  	   <input type="submit" class="button" value="Download" name="download_submit">
  	   <input type="button" class="button" value="Print" name="print_submit" onclick="window.print()" />
- 	 </div>
    </div>
+   <script type="text/javascript" src="../../main.js"></script>
  </body>
 </html>
