9c98c442d1e68ff1f0abe47265ccd75d89663d93
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-26 10:32:42 -0400

    Update session variables

--- phpform/process_forms/process_invitation/process_page_one.php
+++ phpform/process_forms/process_invitation/process_page_one.php
@@ -1,8 +1,5 @@
 <?php
 session_start();
 //include("../../helper_functions/sanitize.php");
-$_SESSION['parents_names'] = $_POST['parents_names'];
-$_SESSION['spouse1'] = $_POST['spouse1'];
-$_SESSION['spouse2'] = $_POST['spouse2'];
 header("Location: ../../customizable_templates/invitations/customize_page_two.php");
 ?>
--- phpform/process_forms/process_invitation/process_page_two.php
+++ phpform/process_forms/process_invitation/process_page_two.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
 //include("../../helper_functions/sanitize.php");
-  $_SESSION['venue'] = $_POST['venue'];
-  $_SESSION['address'] = $_POST['address'];
-  $_SESSION['date'] = $_POST['date'];
-  $_SESSION['time'] = $_POST['time'];
-  header("Location: ../../customizable_templates/invitations/customize_page_three.php");
+header("Location: ../../customizable_templates/invitations/customize_page_three.php");
 ?>
