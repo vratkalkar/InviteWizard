3848646da76d026f792beaea4332a919f1e53374
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-26 09:40:17 -0400

    Remove sanitize helper function and if/else to check getting/setting of session variables

--- phpform/process_forms/process_invitation/process_page_one.php
+++ phpform/process_forms/process_invitation/process_page_one.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-include("../../helper_functions/sanitize.php");
-
-$_SESSION['parents_names'] = sanitize($_POST['parents_names']);
-$_SESSION['spouse1'] = sanitize($_POST['spouse1']);
-$_SESSION['spouse2'] = sanitize($_POST['spouse2']);
+//include("../../helper_functions/sanitize.php");
+$_SESSION['parents_names'] = $_POST['parents_names'];
+$_SESSION['spouse1'] = $_POST['spouse1'];
+$_SESSION['spouse2'] = $_POST['spouse2'];
 header("Location: ../../customizable_templates/invitations/customize_page_two.php");
 ?>
--- phpform/process_forms/process_invitation/process_page_three.php
+++ phpform/process_forms/process_invitation/process_page_three.php
@@ -1,25 +1,15 @@
 <?php
 session_start();
-include("../../helper_functions/sanitize.php");
+//include("../../helper_functions/sanitize.php");
 	$_SESSION['invite_type'] = $_POST['invite_type'];
-	$_SESSION['parents_names'] = sanitize($_POST['parents_names']);
-	$_SESSION['spouse1'] = sanitize($_POST['spouse1']);
-	$_SESSION['spouse2'] = sanitize($_POST['spouse2']);
-	$_SESSION['venue'] = sanitize($_POST['venue']);
-	$_SESSION['address'] = sanitize($_POST['address']);
-	$_SESSION['date'] = sanitize($_POST['date']);
-	$_SESSION['time'] = sanitize($_POST['time']);
-	$_SESSION['website_url'] = sanitize($_POST['website_url']);
-	$_SESSION['wedding_code'] = sanitize($_POST['wedding_code']);
-	
-
-	if ($_POST['invite_type'] == 'formal' ){
-	    header("Location: ../../display_customized_templates/invitation_type/formal_invitation.php");
-	    break;
-	 }
-	 else if ($_POST['invite_type'] == 'casual' ) {
-	 	header("Location: ../../display_customized_templates/invitation_type/casual_invitation.php");
-	    break;
-	 }
-}
+	$_SESSION['parents_names'] = $_POST['parents_names'];
+	$_SESSION['spouse1'] = $_POST['spouse1'];
+	$_SESSION['spouse2'] = $_POST['spouse2'];
+	$_SESSION['venue'] = $_POST['venue'];
+	$_SESSION['address'] = $_POST['address'];
+	$_SESSION['date'] = $_POST['date'];
+	$_SESSION['time'] = $_POST['time'];
+	$_SESSION['website_url'] = $_POST['website_url'];
+	$_SESSION['wedding_code'] = $_POST['wedding_code'];
+	header("Location: ../../display_customized_templates/invitation_type/formal_invitation.php"); 
 ?>
