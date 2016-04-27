43d9ba43099fdbbc97331e47bcaca9f59644f34b
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-27 13:44:06 -0400

    Remove user_theme_selection directory css

--- phpform/customizable_templates/invitations/customize_page_two.php
+++ phpform/customizable_templates/invitations/customize_page_two.php
@@ -19,17 +19,18 @@
 
    <div class="row">
      <div class="form_container">
-      <form action="../../process_forms/process_invitation/process_page_two.php" method="POST" data-abide>
-        <label>Website URL:<br>
-          <input type="text" name="website_url" />
-        </label>
-
-        <label>Wedding Code:<br>
-          <input type="number" name="wedding_code" placeholder="Your Appy Code" />
-        </label>
-        <input type="submit" class="button"  id="continue_button" value="Continue" name="form_submit">
-      </form>
+       <div class="website_code_info">
+        <form action="../../process_forms/process_invitation/process_page_two.php" method="POST" data-abide>
+           <label>Website URL:<br>
+            <input type="text" name="website_url" />
+           </label>
+           <label>Wedding Code:<br>
+            <input type="number" name="wedding_code" placeholder="Your Appy Code" />
+           </label>
+        </div>
+         </form>
        <input type="submit" class="button"  id="back_button" value="Back" onclick="history.back();">
+       <input type="submit" class="button"  id="continue_button" value="Continue" name="form_submit">
     </div>
   </div>
     <script type="text/javascript" src="../../main.js"></script>
