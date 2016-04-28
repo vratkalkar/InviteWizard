0beaab0b0cc195832e956002567ce09636f3a120
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-28 11:13:09 -0400

    Update form action

---
+++ phpform/process_forms/process_invitation/process_page_four.php
@@ -0,0 +1,26 @@
+<?php
+//Get select option on submit and redirect to corresponding page
+if(isset($_POST['form_submit'])){
+	$user_choice = $_POST['who_is_hosting'];
+	 switch($user_choice){
+		case 'couple':
+		  header("Location: ../customizable_templates/invitations/customize_page_one.php");
+		  break;
+		case 'brides_parents':
+		  header("Location: ../customizable_templates/customize_mailing_address.php");
+		  break;
+		case 'grooms_parents':
+		  header("Location: ../customizable_templates/customize_save_the_date.php");
+		  break;
+		case 'notes':
+		  header("Location: ../customizable_templates/customize_note.php");
+		  break;
+		case 'reminder':
+		  header("Location: ../customizable_templates/customize_reminder.php");
+		  break;
+		case 'thank_you_card':
+		  header("Location: ../customizable_templates/customize_thank_you_card.php");
+		  break;
+	}
+ }
+?>
