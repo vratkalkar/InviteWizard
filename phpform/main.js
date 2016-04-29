d5493d2e384496b3efa9bcb0b313643e12e2ebb1
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-29 16:29:42 -0400

    Start working with jsPDF referencing library documentation

--- phpform/main.js
+++ phpform/main.js
@@ -1,6 +1,43 @@
+$(document).ready(function(){
+
 //Add PDF download on "Download" button click with jsPDF
- $('#click_download').on("click", function(e){
- 	e.preventDefault();
- 	console.log("The download button was clicked");
- 	//var pdf = new jsPDF('p', 'mm', 'a4');
- });
+$('#click_download').on("click", function() {
+	console.log("download button clicked");
+	var pdf = new jsPDF('p', 'cm', 'a4');
+	var source = $("html").get('body');
+
+	specialElementHandlers = {
+		'#click_download': function(element, renderer) {
+			return true;
+		}
+	}
+
+	margins = {
+    top: 80,
+    bottom: 60,
+    left: 40,
+    width: 522
+
+   };
+    // all coords and widths are in jsPDF instance's declared units
+    // 'inches' in this case
+    pdf.fromHTML(
+  	  source // HTML string or DOM elem ref.
+  	, margins.left // x coord
+  	, margins.top // y coord
+  	, {
+  		  'width': margins.width // max width of content on PDF
+  		, 'elementHandlers': specialElementHandlers
+  	},
+
+  	function (dispose) {
+  	  // dispose: object with X, Y of the last line add to the PDF
+  	  //          this allow the insertion of new lines after html
+        pdf.save('demo.pdf');
+      },
+  	margins
+  )
+
+  });
+
+});
