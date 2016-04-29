12febcd2cb398d3fe35f7265b9918b61e648549e
Vivek Ratkalkar <vratkalkar@live.com>
2016-04-28 20:48:52 -0400

    Add html/css and php for casual_invitation page

--- phpform/display_customized_templates/invitation_type/casual_invitation.php
+++ phpform/display_customized_templates/invitation_type/casual_invitation.php
@@ -1,25 +1,41 @@
 <?php
   session_start();
 ?>
+
 <html>
 <meta charset="utf-8">
 <head>
-  <link rel="stylesheet" href="../../reset.css" type="text/css">
-  <link rel="stylesheet" href="../../main.css" type="text/css">
-  <link rel="stylesheet" href="../../foundation-6.2.0-essential/css/foundation.css" type="text/css">
-</head>
+  <link rel="stylesheet" href="../../reset.css" type="text/css" />
+  <link rel="stylesheet" href="../../main.css" type="text/css" />
+  <link rel="stylesheet" href="../../foundation-6.2.1-essential/css/foundation.css" type="text/css" /> 
+ </head>
  <body class="template_background">
- <div class="title_bar">APPY COUPLE</div>
+   <div class="title_bar">APPY COUPLE</div>
    <div class="sub_title_bar">Appy Invite Wizard</div>
+
    <div class="header_section">
-      <h3>Your Suggested Invitation</h3>
+     <h3>Your Suggested Invitation Wording:</h3>
    </div>
-   <div class="customized_template_preview_area">
- 	 <?php var_dump($_SESSION); ?>
- 	 <div class="button_area">
- 	   <input type="submit" class="button" value="Download" name="download_submit">
- 	   <input type="button" class="button" value="Print" name="print_submit" onclick="window.print()">
- 	 </div>
+
+   <div class="invite_container">
+     <p><?php echo $_SESSION['parents_names']; ?></p>
+     <p>Invite you to join them in celebrating the wedding of</p>
+     <p><?php echo $_SESSION['spouse1']; ?></p>
+            <p>and</p>
+     <p><?php echo $_SESSION['spouse2']; ?></p>
+            <p>and</p>
+     <p><?php echo $_SESSION['date']; ?></p>
+            <p>at</p>
+     <p><?php echo $_SESSION['venue']; ?></p>
+     <p><?php echo $_SESSION['address']; ?></p>
+     <p>Ceremony begins at <?php echo $_SESSION['time']; ?></p>
+     <p>Visit our website at <?php echo $_SESSION['website_url'], ?>, and use wedding code: <?php echo $_SESSION['wedding_code']; ?></p>
    </div>
+ </body>
+  <div class="button_area">
+     <input type="submit" class="button" value="Download" name="download_submit">
+     <input type="button" class="button" value="Print" name="print_submit" onclick="window.print()" />
+   </div>
+   <script type="text/javascript" src="../../main.js"></script>
  </body>
 </html>
--- phpform/display_customized_templates/invitation_type/formal_invitation.php
+++ phpform/display_customized_templates/invitation_type/formal_invitation.php
@@ -30,6 +30,7 @@
      <p><?php echo $_SESSION['venue']; ?></p>
      <p><?php echo $_SESSION['address']; ?></p>
      <p>Ceremony begins at <?php echo $_SESSION['time'] ?></p>
+     <p>Details are available at <?php echo $_SESSION['website_url'], ?>, with wedding code: <?php echo $_SESSION['wedding_code']; ?></p>
    </div>
  </body>
  	<div class="button_area">
