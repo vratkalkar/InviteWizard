4eb8cce51f4536d77c1d036057a1e963b323a326
Vivek Ratkalkar <vratkalkar@live.com>
2016-05-02 20:36:50 -0400

    adding checks for retina/hdpi

--- phpform/main.js
+++ phpform/main.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
 
- $("#click_download").on('click', function(){
-    html2canvas(document.body, {
-      onrendered: function(canvas) {
-        return Canvas2Image.saveAsPNG(canvas);
-      }
+function isRetina() {
+    var query = '(-webkit-min-device-pixel-ratio: 1.5),\
+        (min--moz-device-pixel-ratio: 1.5),\
+        (-o-min-device-pixel-ratio: 3/2),\
+        (min-device-pixel-ratio: 1.5),\
+        (min-resolution: 144dpi),\
+        (min-resolution: 1.5dppx)';
 
-    });
-    
-  });
+        if (window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia(query).matches)) {
+          return true;
+        }
+        return false;
+}
+
+
+ $("#click_download").on('click', function(){
+    console.log(isRetina());
+  //   html2canvas(document.body, {
+  //     onrendered: function(canvas) {
+  //       return Canvas2Image.saveAsPNG(canvas);
+  //            }
+  //  
+  //      });
+ });
 
 })
 
