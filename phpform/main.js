febaa652f57cf93351a3027dd7092508e271d7a4
Vivek Ratkalkar <vratkalkar@live.com>
2016-05-03 15:41:20 -0400

    Add filesaver.js function to avoid need for server side script to change download file name

--- phpform/main.js
+++ phpform/main.js
@@ -1,56 +1,16 @@
 $(document).ready(function(){
-
  $("#click_download").on('click', function(){
-      html2canvas(document.body, {
-       onrendered: function(canvas) {
-        if (window.devicePixelRatio > 1){
-             var retinaCanvas = toRetina(canvas);
-             return Canvas2Image.saveAsPNG(retinaCanvas);
-        }
-        else {
-            return Canvas2Image.saveAsPNG(canvas);
-        };
-      }
-
-    });
-      
+        console.log("button clicked");
+        html2canvas($('#content'), {
+            allowTaint: false,
+            onrendered: function(canvas) {
+                canvas.toBlob(function(canvas){
+                    saveAs(canvas, "appy-invite.png")
+                });
+            }
+       });
   });
-
-//  //Try to update canvas for hi-res/retina(Apple) displays
-//  function toRetina(canvas) {
-//     var ctx = canvas.getContext('2d');
-//     var canvasWidth = canvas.width;
-//     var canvasHeight = canvas.height;
-
-//     canvas.width = canvasWidth * window.devicePixelRatio;
-//     canvas.height = canvasHeight * window.devicePixelRatio;
-//     canvas.style.width = canvasWidth;
-//     ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
-//     return canvas;
-//  };
-
-// //Function found which checks users display and returns boolean if any conditional matches
-// function isRetina() {
-//     var query = '(-webkit-min-device-pixel-ratio: 1.5),\
-//         (min--moz-device-pixel-ratio: 1.5),\
-//         (-o-min-device-pixel-ratio: 3/2),\
-//         (min-device-pixel-ratio: 1.5),\
-//         (min-resolution: 144dpi),\
-//         (min-resolution: 1.5dppx)';
-
-//         if (window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia(query).matches)) {
-//           return true;
-//         }
-//         return false;
-// }
-
-
-
-
-});
-
-
-
+})
     
 
 
