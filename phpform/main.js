3bc5642d9e37af94fdb212f9ea6e5b520fedf873
Vivek Ratkalkar <vratkalkar@live.com>
2016-05-02 21:29:41 -0400

    Working on toRetina function to try and scale canvas

--- phpform/main.js
+++ phpform/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+//Function found which checks users display and returns boolean if any conditional matches
 function isRetina() {
     var query = '(-webkit-min-device-pixel-ratio: 1.5),\
         (min--moz-device-pixel-ratio: 1.5),\
@@ -16,16 +17,37 @@ function isRetina() {
 
 
  $("#click_download").on('click', function(){
-    console.log(isRetina());
-  //   html2canvas(document.body, {
-  //     onrendered: function(canvas) {
-  //       return Canvas2Image.saveAsPNG(canvas);
-  //            }
-  //  
-  //      });
- });
+      html2canvas(document.body, {
+       onrendered: function(canvas) {
+        if (isRetina(window.devicePixelRatio)){
+             return Canvas2Image.saveAsPNG(toRetina(canvas));
+        }
+        else {
+            return Canvas2Image.saveAsPNG(canvas);
+        };
+       
+     }
+   
+    });
+ 
+  });
 
-})
+ function toRetina(canvas) {
+    var ctx = canvas.getContext('2d');
+    var canvasWidth = canvas.width;
+    var canvasHeight = canvas.height;
+
+    canvas.width = canvasWidth * window.devicePixelRatio;
+    canvas.height = canvasHeight * window.devicePixelRatio;
+    canvas.style.width = canvasWidth;
+    ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    return canvas;
+};
+
+
+
+
+});
 
 
 
