904d3c88d82933173cd26782a265e7d3adb59be1
Vivek Ratkalkar <vratkalkar@live.com>
2016-05-11 14:15:00 -0400

    Working with ajax to send button select value to php session

--- phpform/js/main.js
+++ phpform/js/main.js
@@ -24,17 +24,48 @@ $(document).ready(function(){
          });
     });
 
+   var chosenTemplate = $('.chosen_template');
+   chosenTemplate.selectric();
+
    //Customize Page Three buttons
-   $('#invite_type input').on('click', function(e){
+   $('#invite_type input').on('click', function(e) {
         e.preventDefault();
         console.log("button selected");
-        console.log($(this).val());
-    });
+        var selectedInvite = $(this).val();
+        $.ajax({
+          type: 'POST',
+          url: '../../process_forms/process_invitation/process_page_three.php',
+          data: { 'invite_type': selectedInvite },
+          success: function (selectedInvite) {
+            console.log(selectedInvite);
+          },
+          error: function () {
+            console.log("value NOT added");
+          }
+        });
+      
+   });
+
+
 
    $('#hosting_choice input').on('click', function(e){
         e.preventDefault();
+
+        e.preventDefault();
         console.log("button selected");
-        console.log($(this).val());
+        var selectedHost = $(this).val();
+        $.ajax({
+          type: 'POST',
+          url: '../../process_forms/process_invitation/process_page_three.php',
+          data: { 'choose_host': selectedHost },
+          success: function (selectedHost) {
+            console.log(selectedHost);
+          },
+          error: function () {
+            console.log("value NOT added");
+          },
+
+       });
    });
 
 
