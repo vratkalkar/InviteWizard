3241dce84cea368a16df5d3bad7bfd6250b124af
Vivek Ratkalkar <vratkalkar@live.com>
2016-05-24 14:18:49 -0400

    download.php script ready to check

---
+++ phpform/download.php
@@ -0,0 +1,47 @@
+<?php
+set_time_limit(0); //disable time limit for the script
+
+$path = "../display_customized_templates/invitation_type/brides_parents_formal_invitation.php";
+
+$dl_file = preg_replace("([^\w\s\d\-_~,;:\[\]\(\).]|[\.]{2,})", '', $_GET['download_file']); //file name validation
+$dl_file = filter_var($dl_file, FILTER_SANITIZE_URL); //Remove additonal invalid characters
+
+$fullFilePath = $path.$dl_file;
+
+if ($fd = fopen($fullFilePath, "r")) {
+
+	$fsize = filesize($fullFilePath);
+	$path_parts = pathinfo($fullFilePath);
+	$ext = strtolower($path_parts["extension"]);
+
+	switch ($ext) {
+		case 'php':
+		    header("Content-type: application/pdf");
+		    header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); "use 'attachment' to force file download"
+		    break;
+		    //Additional headers for other content types
+		    default;
+		    header("Content-type: application/octet-stream");
+		    header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
+		    break;	
+	}
+	header("Content-length: $fsize");
+	header("Cache-control: private") //to open files directly
+	while(!feof($fd)) {
+		$buffer = fread($fd, 2048);
+		echo $buffer;
+	}
+}
+fclose($fd);
+exit;
+
+
+
+
+
+
+
+
+
+
+?>
